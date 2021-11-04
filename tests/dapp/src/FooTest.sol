df972665072fbcf4f5219c98e2a39627a8d594a4
Odysseas Lamtzidis <odyslam@gmail.com>
2021-11-04 12:28:09 +0300

    add tests

---
+++ tests/dapp/src/FooTest.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.1;
+import "./console.sol";
+
+contract DsTestMini {
+    bool public failed;
+
+    function fail() private {
+        failed = true;
+    }
+
+    function assertEq(uint a, uint b) internal {
+        if (a != b) {
+            fail();
+        }
+    }
+}
+
+contract FooTest is DsTestMini {
+    uint256 x;
+
+    function setUp() public {
+        x = 1;
+        console.log("this is a great %s", "greeting");
+    }
+
+    function testX() public {
+        require(x == 1, "x is not one");
+    }
+
+    function testFailX() public {
+        assertEq(x, 2);
+    }
+}
